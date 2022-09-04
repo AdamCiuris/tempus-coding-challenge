7bcb659d0f843f4b91c1cf7db752ff26221fe7ed
Adam Ciuris <adamciuris@gmail.com>
2022-09-04 18:00:52 -0500

    fix for DNA complements again

--- task_02/BioInformatics.jl
+++ task_02/BioInformatics.jl
@@ -15,7 +15,7 @@ comps = Dict('a' => 't', 'A' => 'T', 'c' => 'g', 'C' => 'G', 't' => 'a', 'T' => 
 calls standard library reverse for string reversal
 =#
 function reverse(strand::String) 
-	return Base.reverse(strand) # there are apparently no allocations in this
+	return Base.reverse(strand) 
 end
 
 	
@@ -26,7 +26,7 @@ note that a complement is upside down and needs to be reversed
 function complement(strand::String) 
 	n = ""
 	
-		for s in Base.reverse(strand)
+		for s in Iterators.reverse(strand) # for "upside-down" dna strand
 			n = n*comps[s] # concats each key value bind from comps dict
 		end
 	return n
