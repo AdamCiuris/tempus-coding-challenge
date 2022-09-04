9ecfdebb2f199a9f10e391945d9eb1130330771d
Adam Ciuris <adamciuris@gmail.com>
2022-09-03 22:07:15 -0500

    nearly there

--- task_02/BioInformatics.jl
+++ task_02/BioInformatics.jl
@@ -6,14 +6,24 @@ using InteractiveUtils
 
 # ╔═╡ aa96bc58-2be4-11ed-3820-656ec59cf1a9
 begin
+
+# map of complements for DNA pairs	
+comps = Dict("a" => "t", "A" => "T", "c" => "g", "C" => "G", "t" => "a", "T" => "A", "g" => "c", "G" => "C")
 ▷(x, y) = y(x) 
-function reverse(p) 
-	return p
+function reverse(strand::String) 
+	return strand
 end
 
-function compliment(p) 
 	
-	return p
+#=
+returns the complements of the given dna strand
+=#
+function complement(strand::String) 
+	for s in strand
+		
+		println(comps[s])
+	end
+	return strand
 end
 
 #=
@@ -21,17 +31,16 @@ returns the pairs for a given half strand of DNA
 followed by a line break \n
 followed by the given string on the new line.
 =#
-function dsDNA(strand)
-	upper = strand ▷ compliment ▷ reverse
+function dsDNA(strand::String)
+	upper = strand ▷ complement ▷ reverse
 	lower = strand
 	return (upper * "\n" * lower)
 end
-end #module
+end #first begin
 
 # ╔═╡ f636964a-b6d3-4df4-aac3-87890d677ece
-
 begin
-	dsDNA("asdf") ▷ Text 
+	dsDNA("atcgatGGGatctgac") ▷ Text 
 end
 
 # ╔═╡ Cell order:
