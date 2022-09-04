7e5e0f680bc9c8aa33afa89007dbe548fed83711
Adam Ciuris <adamciuris@gmail.com>
2022-09-04 18:55:26 -0500

    decorator cmd line interface

--- task_03/init.py
+++ task_03/init.py
@@ -29,10 +29,50 @@ def cat(filename, resources="./resources/"):
 def add(x, y):
     return x+y
 
+@log_it
+def sub(x, y):
+    return x-y
+
+@log_it
+def mult(x, y):
+    return x*y
+
+@log_it
+def div(x, y):
+    return x/y
+
 
 def main():
-    cat("test.txt")
-    add(23, 42)
+    stri = ""
+
+    
+    # print("Please enter a 1 for debug mode or a 0 for no debug logs")
+    # mode =""
+    # mode = str(input())
+    # while (mode != "0" and mode != "1"):
+    #     print("invalid input, try again")
+    #     print("Please enter a 1 for debug mode or a 0 for no debug logs")
+    #     mode = input();
+
+    
+    print("Please enter a 0 followed by a filename located in resources")
+    print("OR enter a 1 followed by add, sub, mult, div for the arithmetic operation")
+    print("follow the operator by the two numbers")
+    print("OR enter exit to quit program")
+    while (stri != 'exit'):
+        stri = str(input())
+        tokens = stri.split()
+        if (tokens[0] == "0"):
+            cat(tokens[1])
+        elif(tokens[0] == "1"):
+            if (tokens[1] == "add"):
+                add(int(tokens[2]), int(tokens[3])) #add
+            elif (tokens[1] == "sub"):
+                sub(int(tokens[2]), int(tokens[3])) #sub
+            elif (tokens[1] == "mult"):
+                mult(int(tokens[2]), int(tokens[3])) #multiply
+            elif (tokens[1] == "div"):
+                div(int(tokens[2]), int(tokens[3])) #divide
 
 
 if __name__ == "__main__":
